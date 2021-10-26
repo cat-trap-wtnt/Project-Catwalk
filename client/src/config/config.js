11ed6b6a3339f3f6fcebcc87206a919f695d8251
Rufus Roffie <jinbl12go@gmail.com>
2021-10-26 16:37:57 -0700

    added get request of products data

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 // Put your API key goes here!
-var API_KEY = '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';
+var API_KEY = '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';
 var ALTELIER_API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax';
 
 module.exports = { API_KEY, ALTELIER_API };
